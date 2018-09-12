3ba8b860523e4b896232ab835f709a7b71a4dc00
Angelica Costantini <angelicacostantini07@gmail.com>
2018-09-12 11:38:26 +0200

    implement test for navbar

--- my_site/spec/feature/index_spec.rb
+++ my_site/spec/feature/index_spec.rb
@@ -1,15 +1,23 @@
 describe 'Index Page', type: :feature do
+
     it 'displays project list' do
         visit '/'
         expect(page).to have_css '.projects'
         
 
         within '.projects' do
-          expect(page).to have_content 'My First Website'
+          expect(page).to have_content 'My First Website in HTML'
           expect(page).to have_content 'FizzBuzz'
+          expect(page).to have_content 'My First Website in React'
+          expect(page).to have_content 'Address Book'
         end
     end
 end
 
-
+    it 'renders navbar partial' do
+    expect(page).to have_selector 'navbar'
+    within 'navbar' do
+        expect(page).to have_content
+    end
+end
 
